7311c42385875bfbe9069a67ee6a17d37d33ef8f
Siddhant Sharma <sharmasiddhant301@gmail.com>
2020-09-18 10:56:18 -0700

    fix interpolation script

--- galen/scripts/interpolate.py
+++ galen/scripts/interpolate.py
@@ -31,8 +31,10 @@ def interpolate(img1, img2, model):
     10 images of interpolation between given images
     """
     model.eval()
-    z1 = model.encode(img1)
-    z2 = model.encode(img2)
+    mu1, logvar1 = model.encode(img1)
+    z1 = model.reparametrize(mu1, logvar1)
+    mu2, logvar2 = model.encode(img2)
+    z2 = model.reparametrize(mu2, logvar2)
 
     factors = np.linspace(1, 0, num=10)
     res = []
@@ -44,18 +46,39 @@ def interpolate(img1, img2, model):
             res.append(im)
     return res
 
+def model_to_resize_dims(model):
+    """
+    Return resizing dimensions based on model type
+
+    Args:
+    * model
+    Type of model
+
+    Returns:
+        Resize dimensions for given model type
+    """
+    if model == "resnet":
+        return (224, 224)
+    elif model == "vgg":
+        return (128, 128)
+    else:
+        raise ValueError("Model must be ResNet or VGG")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Linear Inteprolation between two images")
     parser.add_argument('--image1', required = True, help = 'Image 1')
     parser.add_argument('--image2', required = True, help = 'Image 2')
+    parser.add_argument('--model', required = True, choices = ['resnet', 'vgg'], help = 'Model type to use')
+    parser.add_argument('--dir', required = False, help = 'Path to look for weights for given model')
     parser.add_argument('--weights', required = False, help = 'Path to weights')
-    parser.add_argument('--dir', required = False, default = './training/checkpoints', help = 'Dir to find latest weights')
-    parser.add_argument('--save', required = False, default = './outputs/',help = 'Dir to save interpolation')
+    parser.add_argument('--save', required = False, default = 'galen/outputs/',help = 'Dir to save interpolation')
     args = vars(parser.parse_args())
 
-    if args['weights'] is not None and args['dir'] is not None:
-    	raise ValueError("Both cannot be weights and dir cannot be given at same time")
+    if args['dir'] is not None and args['weights'] is not None:
+    	raise ValueError("Both directory and weights cannot be given at same time")
+    elif args['dir'] is None and args['weights'] is None:
+        raise ValueError("Need to provide a directory or weights")
 
     if not os.path.exists(args['image1']):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
@@ -64,13 +87,15 @@ if __name__ == "__main__":
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
 
     print("Reading images...")
+    resize_dims = model_to_resize_dims(args['model'])
     img1 = cv2.imread(args['image1'])
-    img1 = image_utils.img_to_tensor(img1)
+    img1 = image_utils.img_to_tensor(img1, resize_dims=resize_dims)
     img2 = cv2.imread(args['image2'])
-    img2 = image_utils.img_to_tensor(img2)
+    img2 = image_utils.img_to_tensor(img2, resize_dims=resize_dims)
 
     print("Loading model...")
-    model = model_utils.load_model(weights=args['weights'], dir=args['dir'])
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model_utils.load_model(device, model_name=args['model'], weights=args['weights'], dir=args['dir'])
 
     print("Interpolating...")
     inter = interpolate(img1, img2, model)
