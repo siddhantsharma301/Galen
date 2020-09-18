614779ebb447bb2cc136aee60e0a0cc1655b6f74
Siddhant Sharma <sharmasiddhant301@gmail.com>
2020-09-17 21:23:38 -0700

    refactor report file for readability and functionality

--- galen/scripts/report.py
+++ galen/scripts/report.py
@@ -20,38 +20,70 @@ from galen.utils import model as model_utils
 from galen.utils import plot as plot_utils
 
 
+"""
+Helper functions for report generation
+"""
+def create_parser():
+    """
+    Helper to create an argument parser
+    """
+    parser = argparse.ArgumentParser(description = 'Diagnose chest x-ray; either healthy or unhealthy')
+    parser.add_argument('--image', required = True, help = 'Image to diagnose')
+    parser.add_argument('--model', required = True, choices = ['resnet', 'vgg'], help = 'Model type to use')
+    parser.add_argument('--dir', required = False, default = './training/checkpoints/', help = 'Path to look for weights for given model')
+    parser.add_argument('--weights', required = False, help = 'Path to weights')
+    parser.add_argument('--cuda', required = False, default = 'cpu', help = 'Use CUDA acceleration')
+    return parser
+
+
+def model_to_resize_dims(model):
+    """
+    Return resizing dimensions based on model type
+
+    Args:
+    * model
+    Type of model
+
+    Returns:
+        Resize dimensions for given model type
+    """
+    if model == "resnet":
+        return (224, 224)
+    elif model == "vgg":
+        return (128, 128)
+    else:
+        raise ValueError("Model must be ResNet or VGG")
+
+
 if __name__ == "__main__":
     # Set up threshold values
     SURF_THRESH = 11.5
     KAZE_THRESH = 28.5
 
     # Set up CLI parser
-    parser = argparse.ArgumentParser(description = 'Diagnose chest x-ray; either healthy or unhealthy')
-    parser.add_argument('--image', required = True, help = 'Image to diagnose')
-    parser.add_argument('--weights', required = False, help = 'Path to weights')
-    parser.add_argument('--dir', required = False, default = './training/checkpoints/', help = 'Path to dir for weights')
+    parser = create_parser()
     args = vars(parser.parse_args())
     filename = args['image']
     
-    # Check if image given exists
     if not os.path.exists(args['image']):
-        raise FileNotFoundError(
-                errno.ENOENT, os.strerror(errno.ENOENT), filename)
-
-    if args['weights'] is not None and args['dir'] is not None:
-    	raise ValueError("Both weights and directory cannot be given at same time")
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
+    elif args['dir'] is not None and args['weights'] is not None:
+    	raise ValueError("Both directory and weights cannot be given at same time")
+    elif args['dir'] is None and args['weights'] is None:
+        raise ValueError("Need to provide a directory or weights")
     
     # Start timing
     start = time.time()
 
-    # Read and convert image to tensor
+    # Read and convert image to tensor and resize image
     print("Reading image...")
     img = cv2.imread(args['image'])
-    img = image_utils.img_to_tensor(img)
+    resize_dims = model_to_resize_dims(args['model'])
+    img = image_utils.img_to_tensor(img, resize_dims=resize_dims)
 
     # Load model
     print("Loading model...")
-    model = model_utils.load_model(weights=args['weights'], dir=args['dir'])
+    model = model_utils.load_model(device=args['cuda'], model=args['model'], weights=args['weights'], dir=args['dir'])
 
     # Reconstruct image, convert to tensor, and post-process with blurring
     print("Reconstructing...")
@@ -100,7 +132,3 @@ if __name__ == "__main__":
         print("The KAZE score indicate the image is unhealthy.")
     print("Time taken: {} seconds".format(time.time() - start))
     print('----------------------------------------------------------')
-
-
-    
-    
